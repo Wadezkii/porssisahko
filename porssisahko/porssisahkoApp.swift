914472c6fb02659463d30223af1c50b4dde20777
Weeti <weeti.tiirikainen@hotmail.fi>
2025-01-14 17:58:45 +0100

    Initial Commit

---
+++ porssisahko/porssisahkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  porssisahkoApp.swift
+//  porssisahko
+//
+//  Created by Weeti on 14.1.2025.
+//
+
+import SwiftUI
+
+@main
+struct porssisahkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ porssisahkoUITests/porssisahkoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  porssisahkoUITestsLaunchTests.swift
+//  porssisahkoUITests
+//
+//  Created by Weeti on 14.1.2025.
+//
+
+import XCTest
+
+final class porssisahkoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
